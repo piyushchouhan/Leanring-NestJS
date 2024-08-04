ae919c57da6c641e3e55c397be4b706ace96644e
Piyush Chauhan <piyush9695@gmail.com>
2024-08-04 11:27:27 +0530

    all configuration is added

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [BookmarkModule, UserModule, AuthModule, PrismaModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {
+
+}
